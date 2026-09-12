58634d344cbb7bf0fdbbc7b5bafae7ff90022eb3
SaiMaungLenn <maunglenn@gmail.com>
2026-09-12 18:35:18 +0630

    Updated API_BASE to dynamically resolve server IP on mobile and local network

--- src/store/useErpStore.js
+++ src/store/useErpStore.js
@@ -1,7 +1,9 @@
 import { create } from 'zustand';
 import { addDays, format, parseISO } from 'date-fns';
 
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = typeof window !== 'undefined'
+  ? `http://${window.location.hostname}:4000/api`
+  : 'http://localhost:4000/api';
 
 const defaultVisibleColumns = {
   recordNo: true,
